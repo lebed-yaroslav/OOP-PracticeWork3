131e07b9b1278912d20c231345b72d95649e741b
Лебедь Ярослав <lebed.yaroslav.edu@gmail.com>
2025-11-05 14:17:42 +0700

    cleanup: Remove unused test

--- MathExpr/Program.cs
+++ MathExpr/Program.cs
@@ -34,22 +34,7 @@ internal static class TestProgram
 		IExpr y = new Variable("y");
 		IExpr c = new Constant(10);
 		var inputValues = new Dictionary<string, double> { ["x"] = 1, ["y"] = 2 };
-		var expr1 = Exp(c + Log(x + y));
-		WriteExprInfo(expr1);
-		Console.WriteLine($"-Value (x = 1, y = 2): {expr1.Compute(inputValues)}");
-		Console.WriteLine($"(Simplified): {expr1.Simplify()}");
-		Console.WriteLine($"(Simplified) d/dx: {expr1.Simplify().Deriative("x")}");
-		Console.WriteLine($"d/dx (Non simplified): {expr1.Deriative("x")}");
-		Console.WriteLine($"d/dx (Simplified): {expr1.Deriative("x").Simplify()}");
-	}
-
-	private static void RunDeriativeTest2()
-	{
-		IExpr x = new Variable("x");
-		IExpr y = new Variable("y");
-		IExpr c = new Constant(10);
-		var inputValues = new Dictionary<string, double> { ["x"] = 1, ["y"] = 2 };
-		var expr1 = Exp(Log(x + y));
+		var expr1 = Exp(Log(x + y + c));
 		WriteExprInfo(expr1);
 		Console.WriteLine($"-Value (x = 1, y = 2): {expr1.Compute(inputValues)}");
 		Console.WriteLine($"(Simplified): {expr1.Simplify()}");
